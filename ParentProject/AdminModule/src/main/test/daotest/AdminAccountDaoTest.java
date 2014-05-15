db4823d9406bc66b7dd5427b0bd0ca127ee70d84
Harry Xiong <harryxiong@Harrys-MacBook-Pro.local>
2014-05-15 21:27:55 +0800

    committing dao test

---
+++ ParentProject/AdminModule/src/main/test/daotest/AdminAccountDaoTest.java
@@ -0,0 +1,14 @@
+package daotest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdminAccountDaoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
