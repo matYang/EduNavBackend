1db914b178eb562eff337b907813cdddf70c3814
Harry Xiong <harryxiong@Harrys-MacBook-Pro.local>
2014-05-29 17:29:51 +0800

    server configuration done

--- ParentProject/BaseModule/src/main/java/BaseModule/configurations/ServerConfig.java
+++ ParentProject/BaseModule/src/main/java/BaseModule/configurations/ServerConfig.java
@@ -53,11 +53,9 @@ public class ServerConfig {
 					configurationMap.put("jdbcUri", "as4359fdgk.mysql.rds.aliyuncs.com:3306/db19r3708gdzx5d1?allowMultiQueries=true&&characterSetResults=UTF-8&characterEncoding=UTF-8&useUnicode=yes");
 					configurationMap.put("redisUri", AccessControlCrypto.decrypt("0E0C572F1FE597594C85ED71A04D006F", ac_key, ac_ivy));
 					configurationMap.put("redisSearchHistoryUpbound", "50");
-//					configurationMap.put("sqlPass", AccessControlCrypto.decrypt("A1E4DDE152B755ECC46248A9D629FDD9", ac_key, ac_ivy));
-//					configurationMap.put("sqlUser", AccessControlCrypto.decrypt("7260820C1FAFD1F699249AF73A9D181D7BD6CE549202AD9FE095E1CE635843DB", ac_key, ac_ivy));
-					
-					configurationMap.put("sqlPass", "LIFECENTRICo2o");
-					configurationMap.put("sqlUser", "db19r3708gdzx5d1");
+					configurationMap.put("sqlPass", AccessControlCrypto.decrypt("A1E4DDE152B755ECC46248A9D629FDD9", ac_key, ac_ivy));
+					configurationMap.put("sqlUser", AccessControlCrypto.decrypt("7260820C1FAFD1F699249AF73A9D181D7BD6CE549202AD9FE095E1CE635843DB", ac_key, ac_ivy));
+								
 				}
 			} catch (Exception e){
 				DebugLog.d(e);
