d181819a10d25c5c5198c9c6a0e233de955b60d7
Harry Xiong <harryxiong@Harrys-MacBook-Pro.local>
2014-06-01 16:05:36 +0800

    sdfjk

--- ParentProject/BaseModule/src/main/java/BaseModule/service/FileService.java
+++ ParentProject/BaseModule/src/main/java/BaseModule/service/FileService.java
@@ -18,4 +18,8 @@ public class FileService {
 	public static String uploadLogoImg(int partnerId, File file, String imgName){
 		return AliyunMain.uploadImg(partnerId, file, imgName, ServerConfig.AliyunLogoBucket);
 	}
+	
+	public static String uploadUserFile(int userId,File file, String fileName){
+		return AliyunMain.uploadFile(userId, file, fileName, ServerConfig.AliyunProfileBucket);
+	}
 }
