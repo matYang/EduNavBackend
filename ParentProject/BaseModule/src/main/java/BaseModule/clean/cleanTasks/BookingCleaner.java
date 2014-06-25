411c564c57a954f2767223a10610bfbbc6239ad8
Harry Xiong <xiongchuhanplace@hotmail.com>
2014-06-25 10:17:43 +0800

    booking cleaner

---
+++ ParentProject/BaseModule/src/main/java/BaseModule/clean/cleanTasks/BookingCleaner.java
@@ -0,0 +1,61 @@
+package BaseModule.clean.cleanTasks;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+
+import BaseModule.common.DateUtility;
+import BaseModule.common.DebugLog;
+import BaseModule.configurations.EnumConfig.BookingStatus;
+import BaseModule.eduDAO.BookingDao;
+import BaseModule.eduDAO.EduDaoBasic;
+import BaseModule.model.Booking;
+
+public class BookingCleaner extends BookingDao{
+
+	public static void clean(){
+		Calendar currentDate = DateUtility.getCurTimeInstance();
+		String ct = DateUtility.toSQLDateTime(currentDate);
+		String query = "SELECT * from BookingDao where (status = ? and noRefundDate < ?) or (status = ? and cashbackDate < ?) for update";
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		try{
+			conn = EduDaoBasic.getConnection();
+			conn.setAutoCommit(false);
+
+			stmt = conn.prepareStatement(query);
+			stmt.setInt(1, BookingStatus.started.code);
+			stmt.setString(2,ct);
+			stmt.setInt(3,BookingStatus.succeeded.code);
+			stmt.setString(4, ct);
+			rs = stmt.executeQuery();
+
+			while(rs.next()){
+				Booking booking = BookingDao.createBookingByResultSet(rs, conn);
+				if(booking.getStatus().code == BookingStatus.started.code){
+					booking.setStatus(BookingStatus.succeeded);
+				}else{					
+					booking.setStatus(BookingStatus.consolidated);
+				}
+				BookingDao.updateBookingInDatabases(booking, conn);
+			}
+
+			conn.commit();
+			conn.setAutoCommit(true);
+		}catch (Exception e) {
+			DebugLog.d(e);
+			if (conn != null){
+				try {
+					conn.rollback();
+				} catch (SQLException sql) {
+					DebugLog.d(sql);
+				}
+			}
+		}finally{
+			EduDaoBasic.closeResources(conn, stmt, rs, true);
+		}
+	}
+}
--- ParentProject/BaseModule/src/test/java/BaseModule/cleanerTest/BookingCleanerTest.java
+++ ParentProject/BaseModule/src/test/java/BaseModule/cleanerTest/BookingCleanerTest.java
@@ -1,16 +1,16 @@
 package BaseModule.cleanerTest;
 
+import static org.junit.Assert.fail;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import org.junit.Test;
-
-import BaseModule.clean.cleanTasks.CourseCleaner;
+import BaseModule.clean.cleanTasks.BookingCleaner;
 import BaseModule.common.DateUtility;
 import BaseModule.configurations.EnumConfig.AccountStatus;
 import BaseModule.configurations.EnumConfig.BookingStatus;
 import BaseModule.configurations.EnumConfig.CourseStatus;
-import BaseModule.dbservice.UserDaoService;
 import BaseModule.eduDAO.BookingDao;
 import BaseModule.eduDAO.CourseDao;
 import BaseModule.eduDAO.EduDaoBasic;
@@ -21,6 +21,7 @@ import BaseModule.model.Booking;
 import BaseModule.model.Course;
 import BaseModule.model.Partner;
 import BaseModule.model.User;
+import BaseModule.model.representation.BookingSearchRepresentation;
 
 
 public class BookingCleanerTest {
@@ -90,11 +91,15 @@ public class BookingCleanerTest {
 		String email = "xiongchuhanplace@hotmail.com";
 		int cashbackAmount = 50;	
 		
+		//Nothing happen
 		Booking booking = new Booking(timeStamp,timeStamp, 
 				course.getPrice(), userId, partnerId, courseId, user.getName(), partner.getPhone(),
-				email,partner.getReference(),BookingStatus.succeeded,cashbackAmount);
+				email,partner.getReference(),BookingStatus.awaiting,cashbackAmount);
+		booking.setNoRefundDate(timeStamp);
+		booking.setCashbackDate(timeStamp);
 		BookingDao.addBookingToDatabases(booking);
 		
+		//Nothing happen
 		Calendar finishTime2 = Calendar.getInstance();
 		finishTime2.add(Calendar.DAY_OF_YEAR, -1);			
 		Booking booking2 = new Booking(finishTime2,timeStamp,
@@ -102,24 +107,59 @@ public class BookingCleanerTest {
 				email,partner.getReference()+"2",BookingStatus.cancelled,cashbackAmount);
 		BookingDao.addBookingToDatabases(booking2);
 		
+		//succeeded ---> consolidated
 		Calendar finishTime3 = Calendar.getInstance();
-		finishTime3.add(Calendar.HOUR_OF_DAY, 1);
+		finishTime3.add(Calendar.HOUR_OF_DAY, -1);
 		Booking booking3 = new Booking(finishTime3,timeStamp, 
 				course.getPrice(), userId, partnerId, courseId, user.getName(), partner.getPhone(),
 				email,partner.getReference()+"3",BookingStatus.succeeded,cashbackAmount);
+		booking3.setNoRefundDate(finishTime3);
+		booking3.setCashbackDate(finishTime3);
 		BookingDao.addBookingToDatabases(booking3);
 		
+		//started ---> succeeded
 		Calendar finishTime4 = Calendar.getInstance();
-		finishTime4.add(Calendar.HOUR_OF_DAY, 1);
+		finishTime4.add(Calendar.HOUR_OF_DAY, -1);
 		Booking booking4 = new Booking(finishTime4,timeStamp, 
 				course.getPrice(), userId, partnerId, courseId, user.getName(), partner.getPhone(),
-				email,partner.getReference()+"4",BookingStatus.refunded,cashbackAmount);
-
+				email,partner.getReference()+"4",BookingStatus.started,cashbackAmount);
+		booking4.setNoRefundDate(finishTime4);
+		booking4.setCashbackDate(finishTime4);
 		BookingDao.addBookingToDatabases(booking4);
 		
-		CourseCleaner.cleanCourseRelatedBooking();
-		user = UserDaoService.getUserById(userId);
-		System.out.println(user.toJSON().toString());
+		//Nothing happen
+		Calendar finishTime5 = Calendar.getInstance();
+		finishTime5.add(Calendar.HOUR_OF_DAY, -1);
+		Booking booking5 = new Booking(finishTime4,timeStamp, 
+				course.getPrice(), userId, partnerId, courseId, user.getName(), partner.getPhone(),
+				email,partner.getReference()+"5",BookingStatus.started,cashbackAmount);
+		booking5.setCashbackDate(finishTime5);
+		BookingDao.addBookingToDatabases(booking5);
+		
+		//Nothing happen
+		Calendar finishTime6 = Calendar.getInstance();
+		finishTime6.add(Calendar.HOUR_OF_DAY, -1);
+		Booking booking6 = new Booking(finishTime4,timeStamp, 
+				course.getPrice(), userId, partnerId, courseId, user.getName(), partner.getPhone(),
+				email,partner.getReference()+"6",BookingStatus.succeeded,cashbackAmount);
+		booking6.setNoRefundDate(finishTime6);
+		BookingDao.addBookingToDatabases(booking6);
+		
+		BookingCleaner.clean();
+		
+		ArrayList<Booking> blist = new ArrayList<Booking>();
+		BookingSearchRepresentation sr = new BookingSearchRepresentation();
+		sr.setPartnerId(partnerId);
+		blist = BookingDao.searchBooking(sr);
+		
+		if(blist.get(0).getStatus().code == BookingStatus.awaiting.code &&
+				blist.get(1).getStatus().code == BookingStatus.cancelled.code &&
+				blist.get(2).getStatus().code == BookingStatus.consolidated.code &&
+				blist.get(3).getStatus().code == BookingStatus.succeeded.code &&
+				blist.get(4).getStatus().code == BookingStatus.started.code &&
+				blist.get(5).getStatus().code == BookingStatus.succeeded.code){
+			//Passed;
+		}else fail();
 		
 	}
 }
