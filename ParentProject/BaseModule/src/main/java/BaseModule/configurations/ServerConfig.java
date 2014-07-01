a182ec4b884bbef4f7a2c7bfdd3a859abf42cd6d
matYang <uwse@me.com>
2014-07-01 21:02:27 +0800

    adding more test configurations

--- ParentProject/BaseModule/src/main/java/BaseModule/configurations/ServerConfig.java
+++ ParentProject/BaseModule/src/main/java/BaseModule/configurations/ServerConfig.java
@@ -28,6 +28,7 @@ public final class ServerConfig {
 		
 		
 		static{
+			String modePrefix = "";
 			try{
 				final String value = System.getenv(ENV_VAR_KEY);
 				
@@ -55,11 +56,13 @@ public final class ServerConfig {
 					configurationMap.put("redisUri", "localhost");
 					configurationMap.put("redisPort", "6380");
 					configurationMap.put("memcachedUri", "fdbc1391e96411e3.m.cnhzalicm10pub001.ocs.aliyuncs.com:11211");
-					configurationMap.put("sqlPass", "badstudent");
-					configurationMap.put("sqlUser", "test");
+					configurationMap.put("sqlUser", "082E4185DBC158A01DC2DE32E241AA4C7167BFF2EAD2493A1E95D63496F69CA7");
+					configurationMap.put("sqlPass", "9211A28B9094893E29F00C1072940646");
 					configurationMap.put("sqlMaxConnection","50");
 					configurationMap.put("memcachedUser", "45D65B9EDB590224697482D7525A1612A31A7A0AFE88AE5236E8DA8A85B0145C");
 					configurationMap.put("memcachedPass", "B2E36A9EB801CBD8A55A20B70269755F03E3097CCBECC9BE6CEFC26BFBBB3F12");
+					
+					modePrefix = "test-";
 				}
 				else{
 					//prod env
@@ -79,6 +82,15 @@ public final class ServerConfig {
 				e.printStackTrace();
 				System.exit(1);
 			}
+			
+			userApplicationPrefix = "/"+modePrefix+"api";
+			partnerApplicationPrefix = "/"+modePrefix+"p-api";
+			adminApplicationPrefix = "/"+modePrefix+"a-api";
+			versionPrefix = "/v1.0";
+			
+			AliyunTeacherImgBucket = modePrefix+"teacherimgbucket";
+			AliyunClassroomImgBucket = modePrefix+"classroomimgbucket";		
+			AliyunLogoBucket = modePrefix+"logobucket";
 		}
 		
 		public static void acDecode(String ac_key, String ac_ivy){
@@ -102,11 +114,10 @@ public final class ServerConfig {
 		
 		
 		/*API level constants*/
-		public static final int category_DM = 0;
-		public static final String userApplicationPrefix = "/api";
-		public static final String partnerApplicationPrefix = "/p-api";
-		public static final String adminApplicationPrefix = "/a-api";
-		public static final String versionPrefix = "/v1.0";
+		public static final String userApplicationPrefix;
+		public static final String partnerApplicationPrefix;
+		public static final String adminApplicationPrefix;
+		public static final String versionPrefix;
 		
 		/*Time constants*/
 		public static final String timeZoneIdCH = "asia/shanghai";
@@ -115,9 +126,8 @@ public final class ServerConfig {
 		/* ALIYUN Bucket*/		
 		public static final String AliyunAccessKeyID = "UBnwEnaFUdBewFF9";
 		public static final String AliyunAccessKeySecrete = "L8hyNuKRXo5bfQ9HWURDq0bprDSDYO";
-		public static final String AliyunTeacherImgBucket = "teacherimgbucket";
-		public static final String AliyunClassroomImgBucket = "classroomimgbucket";		
-		public static final String AliyunLogoBucket = "logobucket";
-		public static final String AliyunProfileBucket = "badstudent-aliyun";		
+		public static final String AliyunTeacherImgBucket;
+		public static final String AliyunClassroomImgBucket;		
+		public static final String AliyunLogoBucket;
 		public static final String resourcePrefix = "src/main/resources/";
 }
