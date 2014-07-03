02919b3b6739ab4f0f8e16a906c76f9622f00dc1
matYang <uwse@me.com>
2014-07-03 11:37:07 +0800

    updating models

--- ParentProject/BaseModule/src/main/java/BaseModule/model/ClassPhoto.java
+++ ParentProject/BaseModule/src/main/java/BaseModule/model/ClassPhoto.java
@@ -16,34 +16,32 @@ public class ClassPhoto implements PseudoModel, Serializable{
 	private int partnerId;
 	
 	private String imgUrl;
-	private String intro;
+	private String title;
+	private String description;
 	
 	private Calendar creationTime;
-
-	public ClassPhoto(long classPhotoId, int partnerId, String imgUrl, String intro, Calendar creationTime) {
+	
+	
+	public ClassPhoto(long classPhotoId, int partnerId, String imgUrl, String title, String description, Calendar creationTime) {
 		super();
 		this.classPhotoId = classPhotoId;
 		this.partnerId = partnerId;
 		this.imgUrl = imgUrl;
-		this.intro = intro;
+		this.title = title;
+		this.description = description;
 		this.creationTime = creationTime;
 	}
+	
 
-	public ClassPhoto(int partnerId, String imgUrl, String intro) {
+	public ClassPhoto(int partnerId, String imgUrl, String title,
+			String description) {
 		super();
 		this.classPhotoId = -1;
 		this.partnerId = partnerId;
 		this.imgUrl = imgUrl;
-		this.intro = intro;
+		this.title = title;
+		this.description = description;
 		this.creationTime = DateUtility.getCurTimeInstance();
-	}
-	
-	public long getClassPhotoId() {
-		return classPhotoId;
-	}
-
-	public void setClassPhotoId(long classPhotoId) {
-		this.classPhotoId = classPhotoId;
 	}
 
 	public int getPartnerId() {
@@ -62,26 +60,49 @@ public class ClassPhoto implements PseudoModel, Serializable{
 		this.imgUrl = imgUrl;
 	}
 
-	public String getIntro() {
-		return intro;
+	public String getTitle() {
+		return title;
 	}
 
-	public void setIntro(String intro) {
-		this.intro = intro;
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public long getClassPhotoId() {
+		return classPhotoId;
 	}
 
 	public Calendar getCreationTime() {
 		return creationTime;
 	}
 
-	public void setCreationTime(Calendar creationTime) {
-		this.creationTime = creationTime;
-	}
-
 	@Override
 	public JSONObject toJSON() throws Exception {
 		return ModelReflectiveService.toJSON(this);
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (classPhotoId ^ (classPhotoId >>> 32));
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((imgUrl == null) ? 0 : imgUrl.hashCode());
+		result = prime * result + partnerId;
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -94,21 +115,25 @@ public class ClassPhoto implements PseudoModel, Serializable{
 		ClassPhoto other = (ClassPhoto) obj;
 		if (classPhotoId != other.classPhotoId)
 			return false;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
 		if (imgUrl == null) {
 			if (other.imgUrl != null)
 				return false;
 		} else if (!imgUrl.equals(other.imgUrl))
 			return false;
-		if (intro == null) {
-			if (other.intro != null)
-				return false;
-		} else if (!intro.equals(other.intro))
-			return false;
 		if (partnerId != other.partnerId)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
 			return false;
 		return true;
 	}
-	
 	
 
 
