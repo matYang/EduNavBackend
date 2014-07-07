dd8335980f5407a33ce4be74dd4cfa56fe25c264
matYang <uwse@me.com>
2014-07-07 19:51:34 +0800

    adding partner get by id

--- ParentProject/AdminModule/src/main/java/AdminModule/resources/partner/PartnerIdResource.java
+++ ParentProject/AdminModule/src/main/java/AdminModule/resources/partner/PartnerIdResource.java
@@ -4,21 +4,48 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.restlet.data.MediaType;
 import org.restlet.data.Status;
+import org.restlet.ext.json.JsonRepresentation;
 import org.restlet.representation.Representation;
 import org.restlet.representation.StringRepresentation;
+import org.restlet.resource.Get;
 import org.restlet.resource.Put;
 import AdminModule.resources.AdminPseudoResource;
 import BaseModule.common.DebugLog;
 import BaseModule.dbservice.PartnerDaoService;
 import BaseModule.exception.PseudoException;
 import BaseModule.exception.validation.ValidationException;
+import BaseModule.generator.JSONGenerator;
 
 import BaseModule.model.Partner;
 
 public final class PartnerIdResource extends AdminPseudoResource{
 	private final String apiId = PartnerIdResource.class.getSimpleName();
+	
+	@Get 	    
+    public Representation getPartnerById() {
+        JSONObject jsonObject = new JSONObject();
+        
+        try {
+			int adminId = this.validateAuthentication();
+			int partnerId = Integer.parseInt(this.getReqAttr("id"));
+			DebugLog.b_d(this.moduleId, this.apiId, this.reqId_get, adminId, this.getUserAgent(), String.valueOf(partnerId));
+			
+	    	Partner partner = PartnerDaoService.getPartnerById(partnerId);
+	        jsonObject = JSONGenerator.toJSON(partner);
+	        
+		} catch (PseudoException e){
+			this.addCORSHeader();
+			return this.doPseudoException(e);
+        } catch (Exception e) {
+			return this.doException(e);
+		}
+        Representation result = new JsonRepresentation(jsonObject);
+        this.addCORSHeader();
+        return result;
+    }
 
 	@Put
 	public Representation updatePartner(Representation entity){
